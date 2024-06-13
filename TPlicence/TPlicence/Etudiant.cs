e8c743e5277da5919551a719d25f7e7792976d88
l3l4isesod <genilogicielisesod@gmail.com>
2024-06-13 10:40:28 +0200

    Classe étudiant créée par ASS2 KABANZA JUSTIN, MSc

---
+++ TPlicence/TPlicence/Etudiant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPlicence
+{
+    class Etudiant
+    {
+        private string idEt;
+        private String nomEt;
+        private string postnEt;
+
+        public Etudiant()
+        {
+        }
+    }
+}
