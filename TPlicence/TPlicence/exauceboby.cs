458e9b230ee860aa4dc7a1bdb48f9a83c5e0e789
licenceisesod <finalisterritisesod2024@gmail.com>
2024-06-27 21:54:16 +0200

    BOBY ZAKUDA Exaucé

---
+++ TPlicence/TPlicence/exauceboby.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPlicence
+{
+    class exauceboby
+    {
+        private string idMemoire;
+        private string nomMemoire;
+        private DateTime date_depot;
+        private string description;
+
+
+        public exauceboby()
+        {
+        }
+    }
+}
