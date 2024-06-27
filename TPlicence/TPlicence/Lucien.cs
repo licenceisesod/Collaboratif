940e799c4b18aedb12b49ab02137f9c51417ea2b
licenceisesod <genielogicielisesod@gmail.com>
2024-06-27 15:50:16 +0200

    MUNGURIEK ABEDI JOYCE

---
+++ TPlicence/TPlicence/Lucien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPlicence
+{
+    class Lucien
+    {
+        private string idAnimal;
+        private String nomlAnimal;
+        private string catAnimal;
+
+        public Lucien()
+        {
+        }
+    }
+}
